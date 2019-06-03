3265caeedb57ccb325e1e55925a67f84f35be282
Leonardo Omaña <leotangram@gmail.com>
2019-06-03 08:29:24 -0500

    Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Hola</h1>
+    <div className="App">
+      <Header title='Clima react app' />
+    </div>
   );
 }
 
